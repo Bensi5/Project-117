bdcb4d8f90ef7aba9d149867da012a33e3f418e3
Bensi5 <77843600+Bensi5@users.noreply.github.com>
2021-09-19 21:11:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-//hi
+function preload(){
+
+}
+
+function setup(){
+    canvas=createCanvas(300,300);
+    canvas.center();
+    video=createCapture();
+    video.hide();
+}
+
+function draw(){
+    image(video,0,0,300,300);
+}
