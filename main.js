9e3d50e583fb0c05b39bc18b48ed7021c82a80c7
Bensi5 <77843600+Bensi5@users.noreply.github.com>
2021-10-10 09:22:17 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -7,8 +7,26 @@ function setup(){
     canvas.center();
     video=createCapture();
     video.hide();
+    classifier=ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/g5yX5rkjA/model.json",modelLoaded);
 }
 
 function draw(){
     image(video,0,0,300,300);
+    classifier.classify(video,getResult);
+}
+
+function modelLoaded(){
+    console.log("model is loaded")
+}
+
+function getResult(error,results){
+    if(error){
+        console.log( "error");
+    }
+    else
+    {
+        console.log(results);
+        document.getElementById("person_name").innerHTML-results[0].label;
+        document.getElementById("acurracy").innerHTML=results[0].confidence.toFixed(3);
+    }
 }
